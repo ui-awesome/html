4b7327036c00fd7cca10eaff66dbeeab772f6a71
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2026-02-16 18:19:50 -0300

    feat(form): Add `InputFile` class for HTML `<input type="file">` element with attributes and rendering capabilities. (#46)

---
+++ src/Form/Attribute/HasCapture.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace UIAwesome\Html\Form\Attribute;
+
+use Stringable;
+use UIAwesome\Html\Attribute\Values\Attribute;
+use UnitEnum;
+
+/**
+ * Provides an immutable API for the `capture` attribute.
+ *
+ * @mixin \UIAwesome\Html\Mixin\HasAttributes
+ * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Attributes/capture
+ *
+ * @copyright Copyright (C) 2026 Terabytesoftw.
+ * @license https://opensource.org/license/bsd-3-clause BSD 3-Clause License.
+ */
+trait HasCapture
+{
+    /**
+     * Sets the `capture` attribute.
+     *
+     * Usage example:
+     * ```php
+     * echo \UIAwesome\Html\Form\InputFile::tag()->capture('user')->render();
+     * echo \UIAwesome\Html\Form\InputFile::tag()->capture('environment')->render();
+     * ```
+     *
+     * @param string|Stringable|UnitEnum|null $value Capture value, or `null` to remove the attribute.
+     *
+     * @return static New instance with the updated `capture` attribute.
+     */
+    public function capture(string|Stringable|UnitEnum|null $value): static
+    {
+        return $this->setAttribute(Attribute::CAPTURE, $value);
+    }
+}
---
+++ src/Form/InputFile.php
@@ -0,0 +1,139 @@
+<?php
+
+declare(strict_types=1);
+
+namespace UIAwesome\Html\Form;
+
+use UIAwesome\Html\Attribute\Form\{CanBeMultiple, CanBeRequired, HasAccept, HasForm};
+use UIAwesome\Html\Attribute\Global\{CanBeAutofocus, HasTabindex};
+use UIAwesome\Html\Attribute\Values\Type;
+use UIAwesome\Html\Core\Element\BaseInput;
+use UIAwesome\Html\Form\Attribute\HasCapture;
+use UIAwesome\Html\Form\Mixin\HasUnchecked;
+use UIAwesome\Html\Helper\Naming;
+use UIAwesome\Html\Interop\{VoidInterface, Voids};
+
+/**
+ * Renders the HTML `<input type="file">` element.
+ *
+ * The `<input type="file">` element allows the user to choose one or more files from their device storage. Once chosen,
+ * the files can be uploaded to a server using form submission, or manipulated using JavaScript code and the File API.
+ *
+ * Usage example:
+ * ```php
+ * echo \UIAwesome\Html\Form\InputFile::tag()
+ *     ->accept('image/png, image/jpeg')
+ *     ->name('avatar')
+ *     ->render();
+ * echo \UIAwesome\Html\Form\InputFile::tag()
+ *     ->multiple(true)
+ *     ->name('photos')
+ *     ->render();
+ * echo \UIAwesome\Html\Form\InputFile::tag()
+ *     ->capture(\UIAwesome\Html\Form\Values\Capture::USER)
+ *     ->name('video')
+ *     ->render();
+ * ```
+ *
+ * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/input/file
+ *
+ * @copyright Copyright (C) 2026 Terabytesoftw.
+ * @license https://opensource.org/license/bsd-3-clause BSD 3-Clause License.
+ */
+final class InputFile extends BaseInput
+{
+    use CanBeAutofocus;
+    use CanBeMultiple;
+    use CanBeRequired;
+    use HasAccept;
+    use HasCapture;
+    use HasForm;
+    use HasTabindex;
+    use HasUnchecked;
+
+    /**
+     * Returns the array of HTML attributes for the element.
+     *
+     * @return array Attributes array assigned to the element.
+     *
+     * @phpstan-return mixed[]
+     */
+    public function getAttributes(): array
+    {
+        $attributes = parent::getAttributes();
+        $attributes['name'] = $this->generateNameWithMultiple();
+
+        // value attribute is not allowed for the `<input type="file">` element, so we remove it if it exists.
+        unset($attributes['value']);
+
+        return $attributes;
+    }
+
+    /**
+     * Returns the tag enumeration for the `<input>` element.
+     *
+     * @return VoidInterface Tag enumeration instance for `<input>`.
+     */
+    protected function getTag(): VoidInterface
+    {
+        return Voids::INPUT;
+    }
+
+    /**
+     * Returns the default configuration for the input element.
+     *
+     * @return array Default configuration array with method calls as keys.
+     *
+     * @phpstan-return array<string, mixed>
+     */
+    protected function loadDefault(): array
+    {
+        return [
+            'template' => ['{prefix}\n{unchecked}\n{tag}\n{suffix}'],
+            'type' => [Type::FILE],
+        ] + parent::loadDefault();
+    }
+
+    /**
+     * Renders the `<input>` element with its attributes.
+     *
+     * @return string Rendered HTML for the `<input>` element.
+     */
+    protected function run(): string
+    {
+        $uncheckedValue = $this->getUncheckedValue();
+
+        if ($uncheckedValue !== null && $uncheckedValue !== '') {
+            $unchecked = InputHidden::tag()
+                ->id(null)
+                ->name($this->generateNameWithMultiple())
+                ->value($uncheckedValue)
+                ->render();
+        }
+
+        return $this->buildElement(
+            '',
+            [
+                '{unchecked}' => $unchecked ?? '',
+            ],
+        );
+    }
+
+    /**
+     * Generates the name of the input element, adding `[]` if the `multiple` attribute is set.
+     *
+     * @return string The generated name.
+     */
+    private function generateNameWithMultiple(): string
+    {
+        /** @phpstan-var string $name */
+        $name = $this->getAttribute('name', '');
+        $isMultiple = $this->getAttribute('multiple', false);
+
+        if ($isMultiple === false) {
+            return $name;
+        }
+
+        return Naming::generateArrayableName($name);
+    }
+}
